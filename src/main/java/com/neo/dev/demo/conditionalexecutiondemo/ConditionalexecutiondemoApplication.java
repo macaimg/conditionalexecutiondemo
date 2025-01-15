79972dfd400d0ea9e6f5b73c385c2abddc55cc9e
neodev <neodev31@gmail.com>
2025-01-14 21:53:31 -0800

    feat: initial commit

---
+++ src/main/java/com/neo/dev/demo/conditionalexecutiondemo/ConditionalexecutiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.neo.dev.demo.conditionalexecutiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalexecutiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalexecutiondemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo/dev/demo/conditionalexecutiondemo/ConditionalexecutiondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.dev.demo.conditionalexecutiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalexecutiondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
